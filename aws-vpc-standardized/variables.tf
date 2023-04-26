c78d53838adf55e010cd08157eacb4d2161cd5a2
aditya <fabregas4aditya@gmail.com>
2023-04-26 19:23:52 +0530

    added pre commit hook

--- aws-vpc-standardized/variables.tf
+++ aws-vpc-standardized/variables.tf
@@ -3,18 +3,18 @@
 # it here as well since it will over ride the values
 
 variable "aws_region" {
-  type = string
+  type        = string
   description = "the dev region of aws"
   #default     = "us-east-1"
 }
 
 variable "env" {
-  type = string
+  type        = string
   description = "environment of the resources"
   #default     = "dev"
 }
 
 variable "azs" {
   description = "availability zones"
-  type = list(string)
+  type        = list(string)
 }
