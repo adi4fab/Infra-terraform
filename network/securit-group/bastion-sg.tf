3ffdd7c05a42bc9c7c1e2f121d492dfaeaa4c9f4
aditya <fabregas4aditya@gmail.com>
2023-04-27 22:40:16 +0530

    added backend and sg

---
+++ network/securit-group/bastion-sg.tf
@@ -0,0 +1,11 @@
+module "public-bastion-sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "4.17.2"
+
+  name                = "public-bastion-sg"
+  description         = "Security group for web-server with HTTP and SSH ports open within VPC"
+  vpc_id              = "adawdaw"
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  ingress_rules       = ["ssh-tcp"]
+  egress_rules        = ["all-all"]
+}
