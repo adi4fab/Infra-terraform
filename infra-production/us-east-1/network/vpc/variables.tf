28eedc6932be6df99cef84a7567488deb6704690
aditya <fabregas4aditya@gmail.com>
2023-04-28 12:10:07 +0530

    variable modified

--- infra-production/us-east-1/network/vpc/variables.tf
+++ infra-production/us-east-1/network/vpc/variables.tf
@@ -2,11 +2,6 @@
 # commented default value since you are specifiying in tfvars file, no harm in defining
 # it here as well since it will over ride the values
 
-variable "env" {
-  type        = string
-  description = "environment of the resources"
-  #default     = "dev"
-}
 
 variable "azs" {
   description = "availability zones"
