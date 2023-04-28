2af8598e1fa0596bac1d7dca0d86b482ac04a095
aditya <fabregas4aditya@gmail.com>
2023-04-28 11:50:10 +0530

    refactor the code

--- infra-production/us-east-1/network/securit-group/webserver-sg.tf
+++ infra-production/us-east-1/network/securit-group/webserver-sg.tf
@@ -2,9 +2,12 @@ module "security-group" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.17.2"
 
-  name                = "web-server-sg"
+  name                = "production-web-server-sg"
   description         = "Security group for web-server with HTTP and SSH ports open within VPC"
   vpc_id              = "adawdaw"
   ingress_cidr_blocks = ["10.10.0.0/16"]
   ingress_rules       = ["http-80-tcp", "ssh-tcp"]
+
+  tags = local.common_tags
+
 }
