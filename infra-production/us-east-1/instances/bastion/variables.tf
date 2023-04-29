572697441bb5dd113a620fa0b7b60b61e53bf890
aditya <fabregas4aditya@gmail.com>
2023-04-29 13:51:08 +0530

    added modules and instances

---
+++ infra-production/us-east-1/instances/bastion/variables.tf
@@ -0,0 +1,17 @@
+variable "instance_type" {
+  description = "instacne type of the ec2"
+  type        = string
+}
+
+variable "instance_key" {
+  description = "instacne key of the ec2"
+  type        = string
+}
+
+variable "private_instance_count" {
+  description = "number of instance"
+  type        = number
+}
+
+
+
---
+++ infra-production/us-east-1/instances/webserver/backend.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = "1.2.8"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.35"
+    }
+  }
+  backend "s3" {
+    bucket = "terraform-aditya-state-storage"
+    key    = "prod/instances/webserver/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+provider "aws" {
+  region  = "us-east-1"
+  profile = "default"
+}
+
